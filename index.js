8aef88500ce44c1a8c56a0c91a947c4e66a44c72
y10b <victory.jun01@gmail.com>
2025-01-31 15:37:08 +0900

    Add mul & div

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function subtract(a, b){
     return a-b;
 }
 
+function mul (a,b){
+    return a*b;
+}
+
+function div (a,b){
+    return a/b;
+}
+
 function testAdd(){
     console.log(add(1, 1) ===2);
 }
